4ca6286b7889686ec04e4fba8dc801614a7d2f5e
mrremo1109 <55891429+mrremo1109@users.noreply.github.com>
2024-08-23 01:41:44 +0100

    Final Out

--- jobportal-main/urls.py
+++ jobportal-main/urls.py
@@ -1,28 +1,23 @@
-"""JobPortal URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.conf import settings
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('App.urls'))
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home, name='home'),
+    path('registerRecruiter/', views.registerRecruiter, name='registerRecruiter'),
+    path('loginUser/', views.loginUser, name='loginUser'),
+    path('profile/', views.profile, name='profile'),
+    path('logoutUser/', views.logoutuser, name='logoutUser'),
+    path('apply/<str:job_id>', views.apply, name='apply'),
+    path('addPost/' , views.addPost, name='addPost'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('about/', views.about, name='about'),
+    path('search/', views.searched, name='search'),
+    path('manage_applications/', views.manage_applications, name='manage_applications'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('application/<int:application_id>/update/', views.update_application_status, name='update_application_status'),
+    path('accept_application/<int:application_id>/', views.accept_application, name='accept_application'),
+    path('reject_application/<int:application_id>/', views.reject_application, name='reject_application'),
+    path('chat/', views.chat_view, name='chat_view'),  # This should be the general chat view URL
+    path('chat/<str:email>/', views.chat_view, name='chat_view'),  # This should be for specific chat view
+    path('send_message/', views.send_message, name='send_message'),
+    ]
